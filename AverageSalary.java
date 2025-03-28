5dcaea331a771df8e00553465f83a065512d13c6
Brittney Ramsubick <bramsubic@gmail.com>
2025-03-28 15:35:02 -0400

    initial set up of questions

---
+++ AverageSalary.java
@@ -0,0 +1,12 @@
+class AverageSalary {
+    public double average(int[] salary) {
+        System.out.println("Hello");
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        AverageSalary averageSalary = new AverageSalary();
+        int[] salary = { 4000, 3000, 5000 };
+        System.out.println(averageSalary.average(salary));
+    }
+}
