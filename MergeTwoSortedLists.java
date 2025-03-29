6b5a421cec069092cc0719d547c3785d21d0b5b3
chibuzo <kanuchibuzo200@gmail.com>
2025-03-29 13:20:11 -0400

    Add solution for merge two sorted lists

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -22,3 +22,69 @@ Merge Lists by Comparing Nodes:
 Return the Merged List:
 
 */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MergeTwoSortedLists {
+    
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+
+        ListNode resultList = new ListNode(0);
+        ListNode tempList = resultList;
+
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                while (l2 != null) {
+                    ListNode newNode = new ListNode(l2.val);
+                    tempList.next = newNode;
+                    tempList = tempList.next;
+                    l2 = (l2 != null) ? l2.next : null;
+                }
+            } else if (l2 == null) {
+                while (l1 != null) {
+                    ListNode newNode = new ListNode(l1.val);
+                    tempList.next = newNode;
+                    tempList = tempList.next;
+                    l1 = (l1 != null) ? l1.next : null;
+                }
+            } else {
+                if (l1.val < l2.val) {
+                    ListNode newNode = new ListNode(l1.val);
+                    tempList.next = newNode;
+                    tempList = tempList.next;
+                    l1 = (l1 != null) ? l1.next : null;
+                } else if (l2.val < l1.val) {
+                    ListNode newNode = new ListNode(l2.val);
+                    tempList.next = newNode;
+                    tempList = tempList.next;
+                    l2 = (l2 != null) ? l2.next : null;
+                } else {
+                    ListNode newNodeL1 = new ListNode(l1.val);
+                    tempList.next = newNodeL1;
+                    tempList = tempList.next;
+                    ListNode newNodeL2 = new ListNode(l2.val);
+                    tempList.next = newNodeL2;
+                    tempList = tempList.next;
+                    l1 = (l1 != null) ? l1.next : null;
+                    l2 = (l2 != null) ? l2.next : null;
+                }
+
+            }
+
+        }
+        tempList.next = null;
+        return resultList.next;
+    }
+}
